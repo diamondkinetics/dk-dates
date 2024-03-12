8ad9cb350128f388fab9f47c0a91e70ed6dcc7f8
Ron de las Alas <rdelasalas@diamondkinetics.com>
2024-03-12 17:32:29 -0400

    fix: remove cc for now

--- release.config.js
+++ release.config.js
@@ -36,27 +36,27 @@ module.exports = {
         },
       },
     ],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        /*  
-            use conventionalcommits instead of conventional-changelog-angular (default)
-            to introduce new sections in changelog
-        */
-        preset: "conventionalcommits",
-        presetConfig: {
-          types: [
-            { type: "feat", section: "Features", hidden: false },
-            { type: "fix", section: "Bug Fixes", hidden: false },
-            { type: "docs", section: "Miscellaneous Chores", hidden: false },
-            { type: "chore", section: "Miscellaneous Chores", hidden: false },
-          ],
-        },
-        parserOpts: {
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
-        },
-      },
-    ],
+    // [
+    //   "@semantic-release/release-notes-generator",
+    //   {
+    //     /*  
+    //         use conventionalcommits instead of conventional-changelog-angular (default)
+    //         to introduce new sections in changelog
+    //     */
+    //     preset: "conventionalcommits",
+    //     presetConfig: {
+    //       types: [
+    //         { type: "feat", section: "Features", hidden: false },
+    //         { type: "fix", section: "Bug Fixes", hidden: false },
+    //         { type: "docs", section: "Miscellaneous Chores", hidden: false },
+    //         { type: "chore", section: "Miscellaneous Chores", hidden: false },
+    //       ],
+    //     },
+    //     parserOpts: {
+    //       noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
+    //     },
+    //   },
+    // ],
     ["@semantic-release/npm"],
     [
       "@semantic-release/changelog",
